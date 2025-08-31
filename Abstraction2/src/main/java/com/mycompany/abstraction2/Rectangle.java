94e0ef0d4000c3fd7d3292819fce16ad04672631
ShahriarRahman4 <srrifat578@gmail.com>
2025-08-31 17:07:35 +0600

    Delete Abstraction2 directory

--- Abstraction2/src/main/java/com/mycompany/abstraction2/Rectangle.java
+++
@@ -1,9 +0,0 @@
-
-package com.mycompany.abstraction2;
-
-
-public class Rectangle {
-    
-    Rectangle(d1)
-    
-}
--- Abstraction2/src/main/java/com/mycompany/abstraction2/Test.java
+++
@@ -1,8 +0,0 @@
-
-package com.mycompany.abstraction2;
-
-
-public class Test {
-    
-    
-}
